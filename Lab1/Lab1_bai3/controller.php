763419b29c2e34399d3734e6e9ba7df157ef3d0b
phucbt2706 <trongphuceazy06@gmail.com>
2024-01-06 22:11:00 +0700

    Update lab1

--- Lab1/Lab1_bai3/controller.php
+++ Lab1/Lab1_bai3/controller.php
@@ -2,7 +2,11 @@
 include 'model.php';
 
 $user_name = $_POST['username'] ?? '';
-$user = getUser($user_name );
+$user = getUser($user_name);
+
+if (!$user) {
+    echo 'Không tồn tại username!';
+}
 
 include 'view.php';
 ?>
