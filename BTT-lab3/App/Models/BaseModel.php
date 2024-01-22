bbdbb71eacf91747209d132592c30a08928a4eac
phucbt2706 <trongphuceazy06@gmail.com>
2024-01-22 21:54:10 +0700

    lab_3

--- BTT-lab3/App/Models/BaseModel.php
+++ BTT-lab3/App/Models/BaseModel.php
@@ -4,30 +4,29 @@ namespace App\Models;
 use App\Interfaces\ModelInterfaces;
 
 class BaseModel implements ModelInterfaces{
-    protected $table;
+    public $table;
     public function __construct($table){
         $this->table = $table;
     }
 
     public function create(){
-
+        echo "Create <br>";
     }
 
     public function read($id){
-
+    echo "Read id = $id <br>";
     }
 
     public function update($id, $data){
-
+        echo "Update where id = $id <br>";
     }
     public function delete($id){
-
+        echo "Delete id= $id <br>";
     }
     public function getAll(){
-
+        echo "GetAll <br>";
     }
     public function getOne($id, $condition=[]){
         echo "Get one $id <br>";
-        echo $this->table;
     }
 }
--- BTT-lab3/index.php
+++ BTT-lab3/index.php
@@ -5,5 +5,12 @@ use App\Models\User;
 
 $user = new User('user');
 
-$user->getOne('1','');
+echo "Table name is: " . $user->table ."<br>";
+
+$user->create();
+$user->read(1);
+$user->update(2,'');
+$user->delete(3);
+$user->getAll();
+$user->getOne(4);
 
